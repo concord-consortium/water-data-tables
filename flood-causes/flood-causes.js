b0765258a8367753ca91c6646e46b98486b55283
Scott Cytacki <scytacki@concord.org>
2016-06-09 14:26:50 -0400

    Add flood-causes table for water science

---
+++ flood-causes/flood-causes.js
@@ -0,0 +1,18 @@
+var headings = [ 'Cause of Flood', 'Factors Contributing to Flood'];
+
+var labels = [
+'Snowmelt',
+'Flash Flood',
+'New Development',
+'Tropical Storms'
+]
+
+var data = [];
+
+var table = setupTable({
+  data: data,
+  headings: headings,
+  headingWidths: [250, 400],
+  labels: labels,
+  charts: []
+})
